b62546c4c27a98c91e42b5baebb2ad2119577a3b
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-01-06 19:19:45 -0300

    fix(preact): set a fixed preact version

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { withContentlayer } = require('next-contentlayer')
  * @type {import('next').NextConfig}
  */
 module.exports = withContentlayer()({
-  swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: [
@@ -31,7 +30,6 @@ module.exports = withContentlayer()({
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
         react: 'preact/compat',
         'react-dom': 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils'

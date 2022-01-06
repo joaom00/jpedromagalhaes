a1b22145f7a24fcc966410e2c1c0a7472be7d682
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-01-06 19:29:07 -0300

    chore: remove preact

--- next.config.js
+++ next.config.js
@@ -27,15 +27,6 @@ module.exports = withContentlayer()({
       ]
     })
 
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom': 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils'
-      })
-    }
-
     return config
   }
 })

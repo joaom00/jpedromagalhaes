ac225a5b3432a5b30db7df8f2058a188115b72e4
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-01-07 20:10:12 -0300

    fix: sidebar bg in light theme

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -168,7 +168,7 @@ export default function Sidebar() {
         <nav
           className={`${
             navigation.open ? 'absolute inset-y-0 left-0 translate-x-0 shadow-lg' : 'absolute -translate-x-full'
-          } lg:relative flex flex-none flex-col lg:translate-x-0 w-3/4 sm:w-1/2 md:w-1/3 lg:w-56 2xl:w-72 3xl:w-80 z-30 lg:z-auto max-h-screen h-full min-h-screen overflow-y-auto transition duration-200 ease-in-out transform border-r pb-10 sm:pb-0 border-gray-200 dark:border-gray-800 dark:bg-gray-900`}
+          } lg:relative flex flex-none flex-col lg:translate-x-0 w-3/4 sm:w-1/2 md:w-1/3 lg:w-56 2xl:w-72 3xl:w-80 z-30 lg:z-auto max-h-screen h-full min-h-screen overflow-y-auto transition duration-200 ease-in-out transform border-r pb-10 sm:pb-0 border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900`}
         >
           <div
             className={`sticky top-0 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800 p-3 space-y-3 ${glassEffect}`}

147ed5c53095ecc21d6f2ef4556a7d7c5abd07e4
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-01-09 10:25:52 -0300

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const withPlugins = require('next-compose-plugins')
+
 const { withContentlayer } = require('next-contentlayer')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = withContentlayer()({
+module.exports = withPlugins([[withBundleAnalyzer], [withContentlayer]], {
   reactStrictMode: true,
   swcMinify: true,
   images: {
